2c7d8e336b796de3efe8a1dfb1c50ae184dc3a5c
Anastasia <asavina@yandex.ru>
2022-12-07 22:54:43 +0300

    new

--- zadanie_3.py
+++ zadanie_3.py
@@ -1,1 +1,17 @@
-тест
+# 3) Реализовать функцию my_func(), которая принимает три позиционных аргумента,
+# и возвращает сумму наибольших двух аргументов.
+#
+def my_func(*elements):
+    spisok = list(elements)
+    iterator = 0
+    result = 0
+    while iterator != 2:
+        maximum = max(spisok)
+        result = maximum + result
+        spisok.remove(maximum)
+        iterator = iterator + 1
+    print(result)
+
+
+my_func(3, 6, 7)
+
